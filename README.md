f0bb05f6a645ee4af5f83711da29e31fe783ee76
David Tay <davidt@aydus.com>
2015-02-18 17:04:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Form Forwarder
+==============
+
+Lightweight module to create a form in a CMS page and submit anywhere.
+
+How to use
+----------
+
+Upload the files to your website, turn off compilation, clear cache, etc. 
+
+Create a form in a CMS page. Form method is POST and action is "formforwarder." 
+
+Add the required hidden field *template_code.* Optional fields include *forward,* which if it is not set, will use
+the default Contacts email (in the admin under System -> Configuration -> General -> Contacts -> Email Options), 
+and *redirect* which is the page to redirect to after form submission, such as a CMS thank you page.
+
+```
+<form id="form-forwarder-example-form" action="{{store url="formforwarder"}}" enctype="multipart/form-data" method="post">
+	<div class="fieldset">
+		<input type="hidden" name="forward" value="davidt@aydus.com" /> 
+		<input type="hidden" name="template_code" value="Form Forwarder Example Form" /> 
+		<input type="hidden" name="redirect" value="thanks" /> 
+		<input type="hidden" name="MAX_FILE_SIZE" value="25000000">
+
+		<div>
+			<label for="name">Name</label>
+			<div class="input-box">
+				<input type="text" name="name" class="input-text required-entry" />
+			</div>
+		</div>
+		<div>
+			<label for="email">Email</label>
+			<div class="input-box">
+				<input type="text" name="email" class="input-text required-entry" />
+			</div>
+		</div>
+		<div>
+			<label for="file">Photo</label>
+			<div class="input-box">
+				<input type="file" name="photo" />
+			</div>
+		</div>
+
+		<div class="buttons-set">
+			<button type="submit" title="submit" class="button">
+				<span><span>Submit</span></span>
+			</button>
+		</div>
+	</div>
+</form>
+
+<script type="text/javascript">
+    //< ![CDATA[
+        var formForwarderExampleForm = new VarienForm('form-forwarder-example-form');
+    //]]>
+</script>
+```
+
+The hidden field *template_code* is the name of the transactional email that will be used. Create the transactional email:
+
+<img src="md/email.png" />
+
+
---
+++ app/code/community/Aydus/FormForwarder/controllers/IndexController.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * FormForwarder controller
+ *
+ * @category   Aydus
+ * @package    Aydus_FormForwarder
+ * @author     Aydus Consulting <davidt@aydus.com>
+ */
+
+class Aydus_FormForwarder_IndexController extends Mage_Core_Controller_Front_Action
+{
+    const XML_PATH_EMAIL_RECIPIENT  = 'contacts/email/recipient_email';
+    const XML_PATH_EMAIL_SENDER     = 'contacts/email/sender_email_identity';
+    
+    public function indexAction()
+    {
+        $refererUrl = $this->_getRefererUrl();
+        $post = $this->getRequest()->getPost();
+        
+        if ( $post ) {
+            
+            $translate = Mage::getSingleton('core/translate');
+            $translate->setTranslateInline(false);
+            
+            try {
+                
+                $postObject = new Varien_Object();
+                $postObject->setData($post);
+                
+                $templateCode = $post['template_code'];
+                $emailTemplate = Mage::getModel('adminhtml/email_template');
+                $emailTemplate->load($templateCode,'template_code');
+                $emailTemplateId = $emailTemplate->getId();
+                
+                $storeId = Mage::app()->getStore()->getId();
+                $recipient = (Zend_Validate::is(trim($post['forward']), 'EmailAddress')) ? $post['forward'] : Mage::getStoreConfig(self::XML_PATH_EMAIL_RECIPIENT, $storeId);
+                
+                $mailTemplate = Mage::getModel('core/email_template');
+                $mailTemplate->setDesignConfig(array('area' => 'frontend'))->setReplyTo($post['Email']);                
+                
+                if (is_array($_FILES) && count($_FILES) > 0){
+                    
+                    foreach ($_FILES as $file){
+                        
+                        foreach ($file['name'] as $i=>$filename){
+                            
+                            $error = $file['error'][$i];
+                            
+                            if (!$error){
+                                
+                                $type = $file['type'][$i];
+                                $tmpName = $file['tmp_name'][$i];
+                                $fileContents = file_get_contents($tmpName);
+                                $attachment = $mailTemplate->getMail()->createAttachment($fileContents,$type,Zend_Mime::DISPOSITION_ATTACHMENT, Zend_Mime::ENCODING_BASE64, $filename);
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                $mailTemplate
+                    ->sendTransactional(
+                        $emailTemplateId,
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SENDER),
+                        $recipient,
+                        null,
+                        array('data' => $postObject)
+                    );
+
+                if (!$mailTemplate->getSentSuccess()) {
+                    throw new Exception();
+                }
+
+                $translate->setTranslateInline(true);
+
+                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
+                
+                $redirect = ($post['redirect']) ? Mage::getUrl($post['redirect']) : $refererUrl;
+                
+                $this->getResponse()->setRedirect($redirect);
+                return;
+                
+            } catch (Exception $e) {
+                
+                $translate->setTranslateInline(true);
+
+                Mage::getSingleton('customer/session')->addError(Mage::helper('contacts')->__('Unable to submit your request. Please, try again later'));
+                $this->getResponse()->setRedirect($refererUrl);
+                return;
+            }
+
+        } else {
+            
+            $this->getResponse()->setRedirect($refererUrl);
+            
+        }
+        
+    }
+
+}
